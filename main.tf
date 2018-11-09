f3419e8164ff6f032759aeff18f4d5cacb5d6e53
John Barnes <jbarnes7952@gmail.com>
2018-11-09 15:56:19 -0500

    test

--- main.tf
+++ main.tf
@@ -4,17 +4,20 @@ terraform {
 	}
 }
 
+data "terraform_remote_state" "tls_private_key" {
+	  backend = "atlas"
+		config {
+			    name = "jb-testing-test3"
+					  
+		}
 
+}
 provider "acme" {
   server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
-
 resource "acme_registration" "reg" {
-  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
+  account_key_pem = "${data.terraform_remote_state.tls_private_key.private_key.private_key_pem}"
 	email_address   = "jbarnes7952@gmail.com"
 }
 
